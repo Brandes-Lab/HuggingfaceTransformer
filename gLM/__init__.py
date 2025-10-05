4cfcc85eb421625cca0573de92f9492d9d8b84c1
Ben Levy <benjaminjslevy@gmail.com>
2025-10-05 16:28:13 +0000

    delete author from init

--- gLM/__init__.py
+++ gLM/__init__.py
@@ -5,12 +5,6 @@ A transformer-based package for protein and gene sequence modeling.
 """
 
 __version__ = "0.1.0"
-__author__ = "Benjamin Levy"
-__email__ = "bl3037@nyu.edu"
 
 # Package metadata
-__all__ = [
-    "__version__",
-    "__author__",
-    "__email__",
-]
+__all__ = ["__version__"]
